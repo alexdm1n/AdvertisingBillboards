fa803e79855eb09d83569c72e179a1802e879de7
Alexey Sys <mrdminyt@gmail.com>
2022-11-05 13:58:52 +0300

    Add Devices view for UserController

---
+++ AdvertisingBillboards/AdvertisingBillboards/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdvertisingBillboards.Controllers;
+
+public class UserController : Controller
+{
+    public IActionResult Devices()
+    {
+        return View();
+    }
+}
