a40187e794a6469591505458e2b08bc6f8638754
Yarmmax <maxim_yarmaluk@mail.ru>
2022-11-25 22:58:49 +0300

    Impelement Service DeviceGroup

---
+++ AdvertisingBillboards/Src/AdvertisingBillboards/Services/IDeviceGroupService.cs
@@ -0,0 +1,19 @@
+using AdvertisingBillboards.Models.Models;
+
+namespace AdvertisingBillboards.Src.AdvertisingBillboards.Services;
+
+public interface IDeviceGroupService
+{
+    IEnumerable<DeviceGroup> Get(long? userId = null);
+    
+    void Add(long id);
+    
+    void Delete(long id);
+    
+    void AddDeviceInGroup(long deviceGroupId, long deviceId);
+    
+    void AddFrequency(long deviceGroupId, int frequence);
+    
+    void DeleteDeviceInGroup(long deviceGroupId, long deviceId);
+    
+}
